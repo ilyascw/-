5fa3e82a15c78f02f1e77cb156963cb8121c2780
ilyascw <162372301+ilyascw@users.noreply.github.com>
2024-10-27 08:49:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,23 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'scikit-learn',
-        'pandas',
-        'numpy',
-        'xgboost',
+        'scikit-learn',   # Библиотека для машинного обучения
+        'pandas',         # Библиотека для работы с данными
+        'numpy',          # Библиотека для численных вычислений
+        'xgboost',        # Библиотека для градиентного бустинга
+        'matplotlib',     # Библиотека для визуализации данных
+        'seaborn',        # Библиотека для улучшенной визуализации
+        'scipy',          # Библиотека для научных вычислений
+        'statsmodels',    # Библиотека для статистического моделирования
+        'imbalanced-learn',  # Библиотека для работы с несбалансированными данными
+        'jupyter',        # Библиотека для работы с ноутбуками Jupyter
+        'notebook',       # Библиотека для запуска Jupyter Notebook
+        'pytest',         # Библиотека для тестирования
+        'joblib',         # Библиотека для сохранения объектов
     ],
     entry_points={
         'console_scripts': [
-            'retirement-predict=retirement_pipeline.pipeline:main',
+            'retirement-predict=retirement_prediction.pipeline:main',
         ],
     },
 )
